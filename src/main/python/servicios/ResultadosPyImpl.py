6d1c44a847b236776dc1f2d9187237dbc52cedd0
Rafael Rojas Crespo <i42rocrr@gmail.com>
2024-02-13 19:59:23 +0100

    Buena pinta resultados distancia euclidea

--- src/main/python/servicios/ResultadosPyImpl.py
+++ src/main/python/servicios/ResultadosPyImpl.py
@@ -1,20 +1,24 @@
 import numpy as np
 
+def generaArray(listaFarmacosPaciente, listaFarmacosLaboratorio):
+    return np.array(list(map(lambda farmaco: farmaco in listaFarmacosPaciente, listaFarmacosLaboratorio)))
 
-print("Ejecución desde Python")
-print("###############################################################")
+def generalListaNombreFarmacosPaciente(elementoPaciente):
+    listaNombreFarmacosPaciente=[]
+    for farmaco in elementoPaciente.getFarmacosPacientesList():
+        listaNombreFarmacosPaciente.append(farmaco.getNombre())
+    return listaNombreFarmacosPaciente
 
-print("PACIENTES:")
-for paciente in todosPacientes:
-    print("Paciente: "+paciente.getNombre()+" ("+ str(paciente.getId()) +"):")
-    print("   Farmacos:")
-    for farmaco in paciente.getFarmacosPacientesList():
-        print(" -"+farmaco.getNombre()+" (" +str(farmaco.getId()) + ")")
-
-
-print("LABORATORIOS:")
 for laboratorio in todosLaboratorios:
-    print("Laboratorio: "+laboratorio.getNombre()+" ("+ str(laboratorio.getId()) +"):")
-    print("   Farmacos:")
+    laboratorio_vector = np.array(list(map(lambda farmaco: laboratorio.getNombre(), laboratorio.getFarmacosLaboratoriosList())))
+
+
+    listaNombreFarmacosLaboratorio=[]
     for farmaco in laboratorio.getFarmacosLaboratoriosList():
-        print(" -"+farmaco.getNombre()+" (" +str(farmaco.getId()) + ")")
+        listaNombreFarmacosLaboratorio.append(farmaco.getNombre())
+
+    pacientes_vectores = {
+        paciente.getNombre(): generaArray(generalListaNombreFarmacosPaciente(paciente), listaNombreFarmacosLaboratorio)
+        for paciente in todosPacientes
+    }
+
